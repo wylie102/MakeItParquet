17c3b13a290a68628b7b9ba91eca9f2d499566bd
wylie102 <wylie102@gmail.com>
2025-03-18 12:16:43 +0000

    Converted dictionaries to NamedTuples

--- Make_It_Parquet/conversion_data.py
+++ Make_It_Parquet/conversion_data.py
@@ -3,47 +3,52 @@
 Module that defines conversion tasks for Make-it-Parquet! using a direct factory function.
 """
 
+import re
+from typing import NamedTuple
 import uuid
+from pathlib import Path
 
-TYPE_CHECKING = False
-if TYPE_CHECKING:
-    from pathlib import Path
+
+class _SQLReadStatementMapping(NamedTuple):
+    csv: str = "read_csv"
+    tsv: str = "read_csv"
+    txt: str = "read_csv"
+    json: str = "read_json"
+    parquet: str = "read_parquet"
+    xlsx: str = "read_xlsx"
+
+
+class _DefaultArgumentMapping(NamedTuple):
+    csv: str = ""
+    tsv: str = ""
+    txt: str = ""
+    json: str = ""
+    parquet: str = ""
+    xlsx: str = ""
 
 
 class ConversionData:
-    # map extensions to duckdb read statements.
-    extension_to_sql_read_statement_map: dict[str, str] = {
-        ".csv": "read_csv",
-        ".tsv": "read_csv",
-        ".txt": "read_csv",
-        ".json": "read_json",
-        ".parquet": "read_parquet",
-        ".xlsx": "read_xlsx",
-    }
-
-    # map extensions to default arguments
-    # default arguments must start with ', '
-    extension_to_default_arguments_map: dict[str, str] = {
-        ".csv": "",
-        ".tsv": "",
-        ".txt": "",
-        ".json": "",
-        ".parquet": "",
-        ".xlsx": "",
-    }
+    # Store mapping dataclasses as Class Attributes.
+    read_statement_mapping: _SQLReadStatementMapping = _SQLReadStatementMapping()
+    default_argument_mapping: _DefaultArgumentMapping = _DefaultArgumentMapping()
 
     def __init__(self, input_ext: str, file_path: Path) -> None:
-        """Initializes ConversionData dataclass."""
+        """Initializes a ConversionData instance."""
 
         # Initialize input data.
         self.input_ext: str = input_ext
         self.file_path: Path = file_path
-        self.read_function: str = ConversionData.extension_to_sql_read_statement_map[
-            self.input_ext
-        ]
-        self.default_arguments: str = ConversionData.extension_to_default_arguments_map[
-            self.input_ext
-        ]
+
+        # Normalize the extension: remove a leading dot.
+        ext_key: str = re.sub(r"^\.", "", self.input_ext)
+
+        # Get read_function and degault argument strings from dataclasses.
+        self.read_function: str = getattr(
+            ConversionData.read_statement_mapping, ext_key
+        )
+        self.default_arguments: str = getattr(
+            ConversionData.default_argument_mapping, ext_key
+        )
 
         # Create table name.
         self.table_name: str = self._create_unique_table_name()
